c6fcb5bca3a984582375fd5b7bec24d87510f71f
Magaiver <magaivermagalhaes.mm@gmail.com>
2026-01-27 11:31:55 -0300

    chore: add prettier and eslint configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +8,25 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      // Variáveis não usadas
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+
+      // Proibir any
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
 ]);
 
 export default eslintConfig;
