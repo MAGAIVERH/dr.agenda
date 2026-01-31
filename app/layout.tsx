0d9eacc36c6705aad558bcb7824fd64494eb781d
Magaiver <magaivermagalhaes.mm@gmail.com>
2026-01-30 22:22:04 -0300

    feat: ajustar botão de envio no formulário de clínica e atualizar metadados de layout para pt-BR

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Toaster position="bottom-center" richColors theme="light" />
