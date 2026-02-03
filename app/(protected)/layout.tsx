323b4564071eaf54b69f786201b3d8e76f70d83e
Magaiver <magaivermagalhaes.mm@gmail.com>
2026-02-02 21:07:04 -0300

    feat: refactor layout component and add reusable page container components

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,16 +1,15 @@
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+
 import { AppSidebar } from './components/app-sidebar';
 
-const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
+      <main className="w-full">
         <SidebarTrigger />
         {children}
       </main>
     </SidebarProvider>
   );
-};
-
-export default ProtectedLayout;
+}
